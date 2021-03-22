c51d9fa099bb85cdfbed441116270f83cd68740a
mauv2sky <mauv2sky@gmail.com>
2021-03-22 23:45:54 +0900

    Fix day 6 challenge code

--- DAY_6_CHALLENGE/src/index.js
+++ DAY_6_CHALLENGE/src/index.js
@@ -1,29 +1,24 @@
 import "./styles.css";
 
-const selectCountry = document.querySelector(".js-country");
-const USER_LS = "yourCountry";
+const selectCountry = document.querySelector(".js-select");
 
 function handleSelect(event) {
   event.preventDefault();
   const currentCountry = event.target.value;
-  localStorage.setItem(USER_LS, currentCountry);
+  localStorage.setItem("country", currentCountry);
 }
 
 function loadCountry() {
-  const myCountry = localStorage.getItem(USER_LS);
-  if (myCountry === null) {
-    selectCountry.addEventListener("change", handleSelect);
-  } else {
-    for (var i = 0; i < selectCountry.children.length; i++) {
-      if (selectCountry.children[i].value === myCountry) {
-        selectCountry.children[i].setAttribute("selected", "");
-      }
-    }
+  const myCountry = localStorage.getItem("country");
+  if(myCountry){
+    const option = document.querySelector(`option[value=${myCountry}]`);
+    option.selected = true;
   }
 }
 
 function init() {
   loadCountry();
+  selectCountry.addEventListener("change", handleSelect);
 }
 
 init();
